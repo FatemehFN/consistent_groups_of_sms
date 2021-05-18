4055cb1bd61ea0fc8dacc3db4d78508dd9a1e262
Fatemeh S. F. Nasrollahi <fxf32@psu.edu>
2021-05-18 09:35:03 -0400

    Create example_3.py

---
+++ example_3.py
@@ -0,0 +1,31 @@
+
+import model_operations as MO
+import sm_csm_operations as SMO
+
+
+
+plant=30
+pollinator=25
+network_number=266
+file_name=str(plant)+'_'+str(pollinator)+'_'+str(network_number)
+
+
+
+#read the text file of the Boolean model
+f=open('plant_pollinator_models/'+file_name+'_less.txt')
+lines=f.readlines()
+f.close()
+
+f1=open('plant_pollinator_models/'+file_name+'_less.txt')
+rules=f1.read()
+f1.close()
+
+
+
+
+
+#build cycle graph using virtual nodes of expanded network 
+cycle_graph=SMO.cycle_graph_virtual_node_based(lines,write_cycle_graph=True)
+
+
+
