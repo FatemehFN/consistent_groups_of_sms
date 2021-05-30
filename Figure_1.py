e061167e962a573e6d1da1cba5101cbcc42ebb6f
Fatemeh S. F. Nasrollahi <fxf32@psu.edu>
2021-05-30 16:23:01 -0400

    Create Figure_1.py

---
+++ Figure_1.py
@@ -0,0 +1,46 @@
+import model_operations as MO
+import BooleanDOI_processing as BDOIp
+
+
+
+
+file_name='figure_1_Boolean_functions'
+
+
+
+#read the text file of the Boolean model
+f=open('Figures/'+file_name+'.txt')
+lines=f.readlines()
+f.close()
+
+
+
+# build the network from the lines
+Gread, readnodes = BDOIp.form_network(lines, sorted_nodename=False)
+
+
+
+# build the expanded network
+prefix, suffix = 'n', ''
+G_expanded = BDOIp.Get_expanded_network(Gread, prefix=prefix, suffix=suffix)
+
+
+
+# write the expanded network in a gml file that can be opened with YED
+MO.write_g_expanded(G_expanded,model_name=file_name)
+
+
+# calculate the mapping from original node names to index numbers
+mapping = {}  # nodename to number index
+for i, node in enumerate(readnodes):
+    index = prefix + str(i) + suffix
+    mapping[node] = index
+    mapping['~' + node] = '~' + index
+
+
+print('Labels in the expanded network:')
+for item in mapping:
+    print('%s : %s'% (mapping[item],item))
+
+
+
