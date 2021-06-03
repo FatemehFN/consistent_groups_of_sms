a0cdb80b367a74f9b4dbfd1fc7017d58a164d68e
Fatemeh S. F. Nasrollahi <fxf32@psu.edu>
2021-06-02 21:41:22 -0400

    Update example_2.py

--- example_2.py
+++ example_2.py
@@ -1,15 +1,19 @@
+"""
+This script shows the workflow for generating and writing the cycle graph for any imported Boolean model. First the model is read from
+a text file, then it is fed into the function cycle_graph_virtual_node_based() to generate the cycle graph. if write_cycle_graph=True, the
+cycle graph is written in a gml file.
+"""
 
 
 import sm_csm_operations as SMO
 
 
 
-plant=30
-pollinator=25
-network_number=266
+
+plant=20
+pollinator=10
+network_number=987
 file_name=str(plant)+'_'+str(pollinator)+'_'+str(network_number)
-
-
 
 #read the text file of the Boolean model
 f=open('plant_pollinator_models/'+file_name+'_less.txt')
@@ -19,10 +23,6 @@ f.close()
 f1=open('plant_pollinator_models/'+file_name+'_less.txt')
 rules=f1.read()
 f1.close()
-
-
-
-
 
 #build cycle graph using virtual nodes of expanded network
 cycle_graph=SMO.cycle_graph_virtual_node_based(lines,write_cycle_graph=True)
