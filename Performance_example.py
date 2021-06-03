67d32d3540301564e03b560fe179fb70a70f3c45
Fatemeh S. F. Nasrollahi <fxf32@psu.edu>
2021-06-02 21:41:55 -0400

    Update Performance_example.py

--- Performance_example.py
+++ Performance_example.py
@@ -1,3 +1,13 @@
+"""
+This script shows the workflow for finding the number of minimal trap spaces (attractors) using python packages 'PyBoolNet' and
+'SM analysis 2021'. These two packages are downloaded from the corresponding repositories provided in the readme and installed
+and imported. Then the Boolean model is read from a text file. For PyBoolNet, first the rules are converted to the format
+compatible with this package, and then they are given to the function bnet2primes() to find the prime implicants.
+Then trap_spces() is called with the first argument as the primes and the second argument equal to "min" to find minimal trap spaces.
+For 'SM analysis 2021', the same primes are given to from_primes() from the module AttractorRepertoire to find the attractors.
+"""
+
+
 import PyBoolNet
 import StableMotifs as sm
 import relationship_operations as RO
@@ -6,12 +16,11 @@ import time
 
 
 
-plant=30
-pollinator=25
-network_number=266
+
+plant=20
+pollinator=10
+network_number=987
 file_name=str(plant)+'_'+str(pollinator)+'_'+str(network_number)
-
-
 
 #read the text file of the Boolean model
 f=open('plant_pollinator_models/'+file_name+'_less.txt')
@@ -22,9 +31,6 @@ f1=open('plant_pollinator_models/'+file_name+'_less.txt')
 rules=f1.read()
 f1.close()
 
-
-
-
 # number of minimal trap spaces from consistent groups of sms
 start_cg=time.time()
 G_rel,list_of_names=RO.construct_relationships_network(lines,rules,write_cycle_graph=False)
@@ -32,9 +38,6 @@ attrs,len_attrs=AO.consistent_groups_of_sms(G_rel,list_of_names,lines)
 print('number of minimal trap spaces from consistent groups of sms:')
 print(len_attrs)
 end_cg=time.time()
-
-
-
 
 # number of minimal trap spaces from PyBoolNet
 start_mints=time.time()
@@ -46,22 +49,16 @@ print('number of minimal trap spaces from PyBoolNet:')
 print(len(mints))
 end_mints=time.time()
 
-
-
-
 # number of attractors from Stable Motifs 2021
 start_21=time.time()
 sm.Format.pretty_print_prime_rules({k: primes[k] for k in sorted(primes)})
-max_simulate_size = 0
-ar = sm.AttractorRepertoire.from_primes(primes, max_simulate_size=max_simulate_size)
+ar = sm.AttractorRepertoire.from_primes(primes, max_simulate_size=0)
 sum_attrs=0
 for a in ar.attractors:
     sum_attrs+=1
 print('number of minimal trap spaces from Stable Motifs 2021:')
 print(sum_attrs)
 end_21=time.time()
-
-
 
 print('performance summary:')
 print('Consistent groups of stable motifs finished the task in %s seconds' % str(end_cg-start_cg))
